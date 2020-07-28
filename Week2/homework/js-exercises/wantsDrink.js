6bc4d7bd7ced6aee3140cf67ab8824a6f6fb7f3f
rbulbul <29.rbulbul@gmail.com>
2020-07-28 22:28:21 +0200

    changed wantsDrink.js file

--- Week2/homework/js-exercises/wantsDrink.js
+++ Week2/homework/js-exercises/wantsDrink.js
@@ -1,41 +1,11 @@
-'use strict';
-
-const drinkTray = [];
+let drinkTray = [];
 const drinkTypes = ['cola', 'lemonade', 'water'];
 
-for (let i = 0; i < 5; i++) {
-  function counter(drink) {
-    let count = 0;
-    for (let elements of drinkTray) {
-      if (elements === drink) {
-        count = count + 1;
-      }
-    }
-    return count;
-  }
-
-  let cola = counter(drinkTypes[0]);
-  let lemonade = counter(drinkTypes[1]);
-
-  if (cola < 2) {
-    drinkTray.push(drinkTypes[0]);
-  } else if (lemonade < 2) {
-    drinkTray.push(drinkTypes[1]);
-  } else {
-    drinkTray.push(drinkTypes[2]);
+for (let i = 0; i < drinkTypes.length; i++) {
+  drinkTray.push(drinkTypes[i]);
+  drinkTray.push(drinkTypes[i]);
+  if (i === drinkTypes.length - 1) {
+    drinkTray.pop();
   }
 }
-console.log(`“Hey guys, I brought a ${drinkTray}“`);
-
-// This is another way to do this program
-/* for (let i = 0; i < 5; i++) {
-  if (i < 2) {
-    drinkTray.push(drinkTypes[0]);
-  } else if (i === 2 || i < 4) {
-    drinkTray.push(drinkTypes[1]);
-  } else {
-    drinkTray.push(drinkTypes[2]);
-  }
-}
-console.log(drinkTray);
-console.log(`“Hey guys, I brought a ${drinkTray}“`); */
+console.log('Hey guys, I brought a' + ' ' + drinkTray);
