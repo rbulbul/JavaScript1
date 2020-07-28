ddb424de19fc5fc860a7cca793dfcb457fdee42a
rbulbul <29.rbulbul@gmail.com>
2020-07-28 22:32:15 +0200

    chenged variable name

--- Week2/homework/js-exercises/recipeCard.js
+++ Week2/homework/js-exercises/recipeCard.js
@@ -2,9 +2,9 @@
 
 let recipe = {};
 recipe.title = 'Kabap';
-recipe['servings'] = 2;
+recipe.servings = 2;
 recipe.ingredients = ['1 kg meat', '2 bread', '2 eggs', 'spicy'];
 console.log(recipe);
-for (let tarif in recipe) {
-  console.log(`${tarif}: ${recipe[tarif]}`);
+for (let property in recipe) {
+  console.log(`${property}: ${recipe[property]}`);
 }
