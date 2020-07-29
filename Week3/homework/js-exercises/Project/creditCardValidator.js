51abc4fc4a5cd858d46919237144e25691407985
rbulbul <29.rbulbul@gmail.com>
2020-07-29 20:00:28 +0200

    The project was started up to a point

--- Week3/homework/js-exercises/Project/creditCardValidator.js
+++ Week3/homework/js-exercises/Project/creditCardValidator.js
@@ -1,24 +1,42 @@
 'use strict';
 
-const validateCreditNumber = (array) => {
-  let total = 0;
-  const letterNumber = /^[0-9a-zA-Z]+$/;
-  let newArray = array;
-  for (let i = 0; i < newArray.length; i++) {
-    if (newArray.length !== 16) {
-      return 'Input must be 16 characters';
-    } else if (isNaN(array)) {
-      return 'All characters must be numbers';
-    } else if (array[array.length - 1] % 2 !== 0) {
-      return 'The last number must be even';
-    }
-    total += newArray[i];
+// This is the number checker function
+const numChecker = (param) => {
+  let newArray = [];
+  for (let i = 0; i < param.length; i++) {
+    newArray.push(param[i]);
   }
-  if (total > 16) {
-    return 'valid';
+
+  let convertToNumber = newArray.map((item) => {
+    return parseInt(item, 10);
+  });
+
+  // Number.isNaN() method determines whether the passed value is NaN and its type is Number
+  const selectNumber = convertToNumber.filter(function (number) {
+    return !Number.isNaN(number);
+  });
+
+  if (selectNumber.length !== newArray.length) {
+    return `Error: INVALID Card Number! All characters must be numbers!`;
   } else {
-    return 'invalid';
+    return `valid`;
   }
 };
 
-console.log(validateCreditNumber('1111111111111110'));
+// This function checks whether credit card number length has 16 character or not
+const lengthChecker = (cardNumber) => {
+  let charNum = cardNumber;
+  if (charNum.length !== 16)
+    return `Error: INVALID Card Number! Your credit card number must be 16 digits!`;
+};
+
+const validateCreditNumber = (cardNumber) => {
+  // Input must be 16 characters
+  return lengthChecker(cardNumber) || numChecker(cardNumber);
+};
+
+// At least two different numbers should be represented
+
+//The sum of all the numbers must be greater than 16
+
+console.log(validateCreditNumber('11111111111111aa'));
