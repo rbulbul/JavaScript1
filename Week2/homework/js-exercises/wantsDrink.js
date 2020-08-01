c7bdd7ad6c3e381f2f4970ff7588ff741a0f5b26
rbulbul <29.rbulbul@gmail.com>
2020-08-02 00:55:51 +0200

    Mathematical operation adjusted to share the drinks according to exercise

--- Week2/homework/js-exercises/wantsDrink.js
+++ Week2/homework/js-exercises/wantsDrink.js
@@ -1,10 +1,10 @@
 let drinkTray = [];
 const drinkTypes = ['cola', 'lemonade', 'water'];
-
+let friends = 5; // if you make this 6 instead 5, you will see all friends will have the drinks
 for (let i = 0; i < drinkTypes.length; i++) {
   drinkTray.push(drinkTypes[i]);
   drinkTray.push(drinkTypes[i]);
-  if (i === drinkTypes.length - 1) {
+  if (i === drinkTypes.length - 1 && user % 2 !== 0) {
     drinkTray.pop();
   }
 }
